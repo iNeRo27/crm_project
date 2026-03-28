1f59ddf2288c91f38a8dd5556da93df172d122a3
Othman <uthman_11_2011@hotmail.com>
2026-03-28 12:28:05 +0300

    init login

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from models import db, User
+from flask import session   
 
 app = Flask(__name__)
 
@@ -13,10 +14,12 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 # connect db with app
 db.init_app(app)
 
+# home route
 @app.route("/")
 def home():
     return "CRM is running "
 
+# register route
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
@@ -32,6 +35,32 @@ def register():
         return redirect("/")
 
     return render_template("register.html")
+
+# login route
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        name = request.form["name"]
+        password = request.form["password"]
+
+        user = User.query.filter_by(name=name, password=password).first()
+
+        if user:
+            session["user_id"] = user.id
+            session["user_name"] = user.name
+
+            return redirect("/dashboard")
+        else:
+            return "Invalid name or password"
+
+    return render_template("login.html")
+
+@app.route("/dashboard")
+def dashboard():
+    if "user_id" not in session:
+        return redirect("/login")
+
+    return f"Welcome {session['user_name']} "
 
 if __name__ == "__main__":
     with app.app_context():
